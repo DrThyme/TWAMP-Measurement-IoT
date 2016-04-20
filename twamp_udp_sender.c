8dd16ef473d415922b46906885d6c20cf28b210d
Linus <linus.hedberg@live.se>
2016-04-20 11:30:35 +0200

    yolo2

--- twamp_udp_sender.c
+++ twamp_udp_sender.c
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2011, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -30,187 +33,120 @@
 #include "contiki.h"
 #include "lib/random.h"
 #include "sys/ctimer.h"
+#include "sys/etimer.h"
 #include "net/ip/uip.h"
 #include "net/ipv6/uip-ds6.h"
-#include "net/ip/uip-udp-packet.h"
-#include "sys/ctimer.h"
-#ifdef WITH_COMPOWER
-#include "powertrace.h"
-#endif
+#include "net/ip/uip-debug.h"
+
+#include "sys/node-id.h"
+
+#include "simple-udp.h"
+#include "servreg-hack.h"
+
 #include <stdio.h>
 #include <string.h>
 
 #include <twamp.h>
 
-#define UDP_CLIENT_PORT 8765
-#define UDP_SERVER_PORT 5678
+#define UDP_PORT 1234
+#define SERVICE_ID 190
 
-#define UDP_EXAMPLE_ID  190
-
-#define DEBUG DEBUG_PRINT
-#include "net/ip/uip-debug.h"
-
-#ifndef PERIOD
-#define PERIOD 60
-#endif
-
-#define START_INTERVAL		(15 * CLOCK_SECOND)
-#define SEND_INTERVAL		(PERIOD * CLOCK_SECOND)
+#define SEND_INTERVAL		(60 * CLOCK_SECOND)
 #define SEND_TIME		(random_rand() % (SEND_INTERVAL))
-#define MAX_PAYLOAD_LEN		512
 
-static struct uip_udp_conn *client_conn;
-static uip_ipaddr_t server_ipaddr;
-static int seq_id;
+static struct simple_udp_connection unicast_connection;
 static struct sender_unauthenticated_test pkt;
 static struct TWAMPtimestamp t;
-/*---------------------------------------------------------------------------*/
+static unsigned int seq_id;
+static uip_ipaddr_t *addr;
 
-
 /*---------------------------------------------------------------------------*/
-PROCESS(udp_client_process, "UDP client process");
-AUTOSTART_PROCESSES(&udp_client_process);
-/*---------------------------------------------------------------------------*/
-static void
-tcpip_handler(void)
-{
-  char *str;
-
-  if(uip_newdata()) {
-    str = uip_appdata;
-    str[uip_datalen()] = '\0';
-    printf("DATA recv '%s'\n", str);
-  }
-}
+PROCESS(unicast_sender_process, "Unicast sender example process");
+AUTOSTART_PROCESSES(&unicast_sender_process);
 /*---------------------------------------------------------------------------*/
 static void
-send_packet()
+receiver(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
 {
-t.second = 0;
-t.microsecond = 0;
-pkt.SeqNo = seq_id;
-pkt.Timestamp = t;
-pkt.ErrorEstimate = 0;
-
-
-  char buf[MAX_PAYLOAD_LEN];
-
-  seq_id++;
-  PRINTF("DATA send to %d 'Hello %d'\n",
-         server_ipaddr.u8[sizeof(server_ipaddr.u8) - 1], seq_id);
-  sprintf(buf, "Hello %d from the client", seq_id);
-  uip_udp_packet_sendto(client_conn, &pkt , 512,
-                        &server_ipaddr, UIP_HTONS(UDP_SERVER_PORT));
-}
-/*---------------------------------------------------------------------------*/
-static void
-print_local_addresses(void)
-{
-  int i;
-  uint8_t state;
-
-  PRINTF("Client IPv6 addresses: ");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
-    state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused &&
-       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
-      PRINTF("\n");
-      /* hack to make address "final" */
-      if (state == ADDR_TENTATIVE) {
-	uip_ds6_if.addr_list[i].state = ADDR_PREFERRED;
-      }
-    }
-  }
+  printf("Data received on port %d from port %d with length %d\n",
+         receiver_port, sender_port, datalen);
 }
 /*---------------------------------------------------------------------------*/
 static void
 set_global_address(void)
 {
   uip_ipaddr_t ipaddr;
+  int i;
+  uint8_t state;
 
   uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
   uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
   uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
 
-/* The choice of server address determines its 6LoPAN header compression.
- * (Our address will be compressed Mode 3 since it is derived from our link-local address)
- * Obviously the choice made here must also be selected in udp-server.c.
- *
- * For correct Wireshark decoding using a sniffer, add the /64 prefix to the 6LowPAN protocol preferences,
- * e.g. set Context 0 to aaaa::.  At present Wireshark copies Context/128 and then overwrites it.
- * (Setting Context 0 to aaaa::1111:2222:3333:4444 will report a 16 bit compressed address of aaaa::1111:22ff:fe33:xxxx)
- *
- * Note the IPCMV6 checksum verification depends on the correct uncompressed addresses.
- */
- 
-#if 0
-/* Mode 1 - 64 bits inline */
-   uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
-#elif 1
-/* Mode 2 - 16 bits inline */
-  uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
-#else
-/* Mode 3 - derived from server link-local (MAC) address */
-  uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a); //redbee-econotag
-#endif
+  printf("IPv6 addresses: ");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      printf("\n");
+    }
+  }
 }
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(udp_client_process, ev, data)
-{
-  static struct etimer periodic;
-  static struct ctimer backoff_timer;
-#if WITH_COMPOWER
-  static int print = 0;
-#endif
+static void
+send_to(){
 
+t.second = 0;
+t.microsecond = 0;
+pkt.SeqNo = seq_id;
+pkt.Timestamp = t;
+pkt.ErrorEstimate = 0;
+
+      char buf[512];
+      memcpy(&buf, &pkt, sizeof(pkt));
+
+      printf("Sending unicast to ");
+      uip_debug_ipaddr_print(addr);
+      printf("\n");
+      sprintf(buf, "Message %d", seq_id);
+      seq_id++;
+      simple_udp_sendto(&unicast_connection, buf, 512 + 1, addr);
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(unicast_sender_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  static struct etimer send_timer;
+  
   PROCESS_BEGIN();
 
-  PROCESS_PAUSE();
+  servreg_hack_init();
 
   set_global_address();
-  
-  PRINTF("UDP client process started\n");
 
-  print_local_addresses();
+  simple_udp_register(&unicast_connection, UDP_PORT,
+                      NULL, UDP_PORT, receiver);
 
-  /* new connection with remote host */
-  client_conn = udp_new(NULL, UIP_HTONS(UDP_SERVER_PORT), NULL); 
-  if(client_conn == NULL) {
-    PRINTF("No UDP connection available, exiting the process!\n");
-    PROCESS_EXIT();
-  }
-  udp_bind(client_conn, UIP_HTONS(UDP_CLIENT_PORT)); 
-
-  PRINTF("Created a connection with the server ");
-  PRINT6ADDR(&client_conn->ripaddr);
-  PRINTF(" local/remote port %u/%u\n",
-	UIP_HTONS(client_conn->lport), UIP_HTONS(client_conn->rport));
-
-#if WITH_COMPOWER
-  powertrace_sniff(POWERTRACE_ON);
-#endif
-
-  etimer_set(&periodic, SEND_INTERVAL);
+  etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
-    PROCESS_YIELD();
-    if(ev == tcpip_event) {
-      tcpip_handler();
-    }
-    
-    if(etimer_expired(&periodic)) {
-      etimer_reset(&periodic);
-      ctimer_set(&backoff_timer, SEND_TIME, send_packet, NULL);
 
-#if WITH_COMPOWER
-      if (print == 0) {
-	powertrace_print("#P");
-      }
-      if (++print == 3) {
-	print = 0;
-      }
-#endif
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    etimer_reset(&periodic_timer);
+    etimer_set(&send_timer, SEND_TIME);
 
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
+    addr = servreg_hack_lookup(SERVICE_ID);
+    if(addr != NULL) {
+      send_to();
+    } else {
+      printf("Service %d not found\n", SERVICE_ID);
     }
   }
 
